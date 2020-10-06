a0bcf65ff1c76af8cb534d4f254a7e7c0528fef0
PLHB1712289 <57471680+PLHB1712289@users.noreply.github.com>
2020-10-06 21:55:20 +0700

    Updates

--- precache-manifest.44142cc38d9922298266eb6eb9fb96f9.js
+++ precache-manifest.44142cc38d9922298266eb6eb9fb96f9.js
@@ -1,11 +1,11 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "21ed4139415ffc496fd74734722e2f1c",
+    "revision": "beb30545fee5b22accddb6674070fc57",
     "url": "/1712289_tuan_01/index.html"
   },
   {
-    "revision": "a603530f0550f6d80b49",
-    "url": "/1712289_tuan_01/static/css/main.2afb2ac7.chunk.css"
+    "revision": "05b8927dccce7df0afec",
+    "url": "/1712289_tuan_01/static/css/main.c5654022.chunk.css"
   },
   {
     "revision": "c7d427e45d2df4d5b114",
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/1712289_tuan_01/static/js/2.daa6408e.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "a603530f0550f6d80b49",
-    "url": "/1712289_tuan_01/static/js/main.098c9d8b.chunk.js"
+    "revision": "05b8927dccce7df0afec",
+    "url": "/1712289_tuan_01/static/js/main.c6238c21.chunk.js"
   },
   {
     "revision": "552b6ca8e619ca9d3f00",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/1712289_tuan_01/precache-manifest.6a5f6a4a0a5cad126e8ee93b245815cd.js"
+  "/1712289_tuan_01/precache-manifest.44142cc38d9922298266eb6eb9fb96f9.js"
 );
 
 self.addEventListener('message', (event) => {
