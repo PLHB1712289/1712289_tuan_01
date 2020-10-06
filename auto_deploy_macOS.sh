782a8510bd1b3a6964b1ce2709e32dd764056ecb
PLHB1712289 <57471680+PLHB1712289@users.noreply.github.com>
2020-10-06 18:03:24 +0700

    deploy app

--- auto_deploy_macOS.sh
+++ auto_deploy_macOS.sh
@@ -2,7 +2,10 @@ echo "==================="
 echo "= AUTO DEPLOY APP ="
 echo "===================" 
 
+# build app
 npm run deploy
+
+# commit & push github
 git add .
 git commit -m "deploy app"
 git push
