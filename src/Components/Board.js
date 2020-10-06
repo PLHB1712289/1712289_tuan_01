3eb20a4cd77fd02e108d1924b97ee659c2273ae4
PLHB1712289 <57471680+PLHB1712289@users.noreply.github.com>
2020-10-06 17:56:40 +0700

    handle winner

--- src/Components/Board.js
+++ src/Components/Board.js
@@ -2,50 +2,103 @@ import React, { useState } from "react";
 import "../App.css";
 import Square from "./Square";
 
+// <function helper>
+const calculateWinner = (squares) => {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a];
+    }
+  }
+
+  return null;
+};
+// </function helper>
+
 const Board = () => {
   // State
   const [squares, setSquares] = useState(Array(9).fill(null));
   const [isXTurn, setIsXTurn] = useState(true);
+  const [winner, setWinner] = useState(null);
+  const [status, setStatus] = useState("Next player: X");
 
-  // Helper function
+  // <Helper function>
+  // Helper 1: Handle click square
   const handleClick = (i) => {
+    //check winner
+    if (winner) {
+      alert("Choose new game");
+      return;
+    }
+
     //clone squares
     const squaresTemp = squares.slice();
 
-    //change value state squares
+    //change value state squares if possbile
     if (!squaresTemp[i]) {
       isXTurn ? (squaresTemp[i] = "X") : (squaresTemp[i] = "O");
 
-      //update state
+      //calculate winner
+      const winPlayer = calculateWinner(squaresTemp);
+
+      //update state isXTurn, squares, winner, status
       setIsXTurn(!isXTurn);
       setSquares(squaresTemp);
+      setWinner(winPlayer);
+      winPlayer
+        ? setStatus(`Winner: ${winPlayer}`)
+        : setStatus(`Next player: ${isXTurn ? "O" : "X"}`);
     } else {
       //alert error
       alert(`Square ${i} already has a value!!`);
     }
   };
 
+  // Helper 2: create new game
+  const newGame = () => {
+    alert(`New Game?`);
+    setSquares(Array(9).fill(null));
+    setIsXTurn(true);
+    setWinner(null);
+    setStatus("Next player: X");
+  };
+
+  // Helper 3: render square
   const renderSquare = (i) => {
     return <Square value={squares[i]} onClick={() => handleClick(i)} />;
   };
+  // </Helper function>
 
   return (
     <div>
-      <div className="status">Next player: {isXTurn ? "X" : "O"}</div>
+      {winner ? <button onClick={() => newGame()}>New Game</button> : ""}
+      <div className="status">{status}</div>
+
       <div className="board-row">
+        {renderSquare(0)}
         {renderSquare(1)}
         {renderSquare(2)}
-        {renderSquare(3)}
       </div>
       <div className="board-row">
+        {renderSquare(3)}
         {renderSquare(4)}
         {renderSquare(5)}
-        {renderSquare(6)}
       </div>
       <div className="board-row">
+        {renderSquare(6)}
         {renderSquare(7)}
         {renderSquare(8)}
-        {renderSquare(9)}
       </div>
     </div>
   );
