2f238187988fadccd7b05fe128809eb32606136e
PLHB1712289 <57471680+PLHB1712289@users.noreply.github.com>
2020-10-06 16:29:02 +0700

    Updates

---
+++ precache-manifest.25a8e297ae9ff1b046898130d48a0bb7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ba913fd3873540e1bf1e5b1a9d206ef1",
+    "url": "/1712289_tuan_01/index.html"
+  },
+  {
+    "revision": "11200c0d26fb5ac9b544",
+    "url": "/1712289_tuan_01/static/css/main.2afb2ac7.chunk.css"
+  },
+  {
+    "revision": "37625654eb7c9b6d97a8",
+    "url": "/1712289_tuan_01/static/js/2.d9ba7ccd.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/1712289_tuan_01/static/js/2.d9ba7ccd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "11200c0d26fb5ac9b544",
+    "url": "/1712289_tuan_01/static/js/main.e3479b02.chunk.js"
+  },
+  {
+    "revision": "552b6ca8e619ca9d3f00",
+    "url": "/1712289_tuan_01/static/js/runtime-main.3aa350ab.js"
+  }
+]);
